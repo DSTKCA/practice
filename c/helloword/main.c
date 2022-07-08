77e00309941dd47571259fbc40378e05bee03865
dstrmdevhrp <6320634+dstkdevhrp@users.noreply.github.com>
2022-07-08 12:06:39 -0700

    Init with C helloworld with with CMake

---
+++ c/helloword/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  helloword
+//
+//  Created by Henry Pang on 2022/7/7.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
